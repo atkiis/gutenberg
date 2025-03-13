2859a738f0868783e1a945f3c9b9d84df51c9352
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-03-13 16:26:22 +0900

    Add user capabilities check

--- lib/compat/wordpress-6.8/site-preview.php
+++ lib/compat/wordpress-6.8/site-preview.php
@@ -6,7 +6,12 @@ if ( ! function_exists( 'wp_initialize_site_preview_hooks' ) ) {
 	 * This function sets IFRAME_REQUEST to true if the site preview parameter is set.
 	 */
 	function wp_initialize_site_preview_hooks() {
-		if ( ! defined( 'IFRAME_REQUEST' ) && isset( $_GET['wp_site_preview'] ) && 1 === (int) $_GET['wp_site_preview'] ) {
+		if (
+			! defined( 'IFRAME_REQUEST' ) &&
+			isset( $_GET['wp_site_preview'] ) &&
+			1 === (int) $_GET['wp_site_preview'] &&
+			current_user_can( 'edit_theme_options' )
+		) {
 			define( 'IFRAME_REQUEST', true );
 		}
 	}
